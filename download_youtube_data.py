089db3f1a573bf96cf9f59b831a2a67c53a6d8e4
Siyuan Jiang <2203934+sjiang1@users.noreply.github.com>
2020-10-19 00:39:49 +0000

    fixed the script for downloading youtube data; added scripts for whoosh.

--- download_youtube_data.py
+++ download_youtube_data.py
@@ -1,9 +1,12 @@
 import json
 import sys
 import os
+import shutil
 from googleapiclient.discovery import build
 import config
 from tqdm import tqdm
+
+data_dir = 'youtube_data'
 
 def youtube_data(api_key, video_id):
     service = build("youtube", "v3", developerKey=api_key)
@@ -14,12 +17,16 @@ if __name__ == '__main__':
     video_ids_file = sys.argv[1]
     my_api_key = config.api_key
 
-    if not os.path.exists('youtube_data'):
-        os.makedirs('youtube_data')
-        
+    if os.path.exists(data_dir):
+        shutil.rmtree(data_dir)
+
+    os.makedirs(data_dir)
+
     with open(video_ids_file) as f:
         video_ids = f.readlines()
         for video_id in tqdm(video_ids):
+            video_id = video_id.strip()
             result = youtube_data(my_api_key, video_id)
-            with open('youtube_data/' + video_id + '.json', 'w') as f:
+            with open(data_dir+ '/' + video_id + '.json', 'w') as f:
                 json.dump(result, f)
+        
